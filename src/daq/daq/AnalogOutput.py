ff61ef82ee0ba4fa7bc7401a7404a627ab371b69
als8921 <als8921@naver.com>
2024-09-09 17:07:44 +0900

    -10 to 10V

--- src/daq/daq/AnalogOutput.py
+++ src/daq/daq/AnalogOutput.py
@@ -5,19 +5,26 @@ from Automation.BDaq.BDaqApi import BioFailed
 
 deviceDescription = "USB-4716,BID#0"
 AOChannel = 0
-amplitude = 2.5
+amplitude = 10 #Voltage Amplitude
 
 
 def AnalogOut():
     instantAo = InstantAoCtrl(deviceDescription)
+    instantAo.channels[0].valueRange = ValueRange.V_Neg10To10  #Voltage Output Range Setting
+
     i = 0
     while True:
-        data = [amplitude + amplitude * np.sin(i/20000)]
-        i+=1
-        ret = instantAo.writeAny(AOChannel, 1, None, data)
-        if BioFailed(ret):
-            print("Error occurred during writing data.")
+        try :
+            data = [amplitude * np.sin(i/50)]
+            i+=1
+            ret = instantAo.writeAny(AOChannel, 1, None, [data])
+            if BioFailed(ret):
+                print("Error occurred during writing data.")
+                return
+        except:
+            ret = instantAo.writeAny(AOChannel, 1, None, [0])
+            instantAo.dispose()
+            print("System Paused")
             return
-
 if __name__ == '__main__':
     AnalogOut()
