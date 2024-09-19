32089580468ebcbc3392a591792b20f10c42fc07
als8921 <als8921@naver.com>
2024-09-19 10:13:54 +0900

    설계하기

---
+++ src/daq/daq/MotorController.py
@@ -0,0 +1,25 @@
+# import 부분
+import numpy as np
+from Automation.BDaq import *
+from Automation.BDaq.InstantAoCtrl import InstantAoCtrl
+from Automation.BDaq.InstantDoCtrl import InstantDoCtrl
+from Automation.BDaq.BDaqApi import BioFailed
+
+
+# 엔코더 값을 불러오는 부분
+
+
+
+# 아날로그 값을 보내주는 부분
+
+
+
+# 방향 CW/CCW Digital 값을 보내주는 부분
+
+
+
+# PID 제어 부분 Encoder -> 10To10V AnalogOutput
+
+
+
+# 제어 명령을 받는 부분
