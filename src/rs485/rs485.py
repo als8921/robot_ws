6e6847733dde1c0826bc593aaa849391c43ce17d
als8921 <als8921@naver.com>
2024-10-16 16:41:05 +0900

    remode : arduinio Remove feat : rs485 test : rs485통신 제어 성공

--- src/rs485/rs485.py
+++ src/rs485/rs485.py
@@ -1,22 +1,26 @@
 import time
 import struct
 import serial
+import serial.rs485
 
-SERIAL_PORT = '/dev/ttyACM0'
+SERIAL_PORT = '/dev/ttyUSB0'
 BAUDRATE = 19200
 
 class RS485Communication:
     def __init__(self):
-        self.ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1)
+        self.ser = serial.rs485.RS485(SERIAL_PORT, baudrate=BAUDRATE, timeout=1)
+        self.ser.rs485_mode = serial.rs485.RS485Settings()
         self.RMID = 0xb7
-        self.TMID = 0xac
+        self.TMID = 0xb8
         self.ID = 0x01
+        self.fmt = None
 
     def create_cmd_packet(self, RMID, TMID, ID, PARAMETER_ID, DATA_NUMBER, *DATA):
         check_sum = RMID + TMID + ID + PARAMETER_ID + DATA_NUMBER + sum(DATA)
         check_sum = (256 - (check_sum % 256)) % 256
-
+        print(check_sum)
         fmt = 'B' * (len(DATA) + 6)
+        self.fmt = fmt
         
         packet_obj = struct.pack(fmt, 
             RMID,
@@ -27,12 +31,17 @@ class RS485Communication:
             *DATA,
             check_sum)
         return packet_obj
+        
+    def unpack_cmd(self, data):
+        return struct.unpack(self.fmt, data)
+
+
+
 
     def read_data(self):
-        if(self.ser.readable()):
+        if self.ser.readable():
             response = self.ser.readline()
-            print("res : ", response)
-
+            print("Received: ", response)
 
     def send_data(self, data):
         try:
@@ -42,15 +51,16 @@ class RS485Communication:
             print(f"Error sending data: {e}")
 
     def run(self):
-        while True:
-            # 데이터 수신
-            rx_data = self.read_data()
-            if rx_data:
-                print(f"Received: {rx_data}")
+        # while True:
 
-            packet = self.create_cmd_packet(self.RMID, self.TMID, self.ID, 10, 1, 8)
+        for i in range(5):            # 데이터 수신
+            self.read_data()
+
+            # 패킷 생성
+            # packet = self.create_cmd_packet(self.RMID, self.TMID, self.ID, 10, 1, 8)
+            packet = self.create_cmd_packet(183, 184, 1, 244, 4, 200, 0, 0, 0)
             self.send_data(packet)
-            time.sleep(0.1)
+            time.sleep(3)
 
     def close(self):
         self.ser.close()
