6b0ec795b9fc6a1d13ed30a8b3e7b40704a13a65
als8921 <als8921@naver.com>
2024-10-17 13:51:58 +0900

    fix : 아두이노에서 Rx 데이터 그대로 Tx refactor : md400 모듈 함수적용

--- src/rs485/ArduinoSerial/ArduinoSerial.ino
+++ src/rs485/ArduinoSerial/ArduinoSerial.ino
@@ -1,20 +1,14 @@
-String receivedData = ""; // 데이터를 저장할 변수
-
 void setup() {
   Serial.begin(19200);
 }
 
 void loop() {
-  while (Serial.available() > 0) {
-    char incomingByte = Serial.read(); // 한 바이트씩 읽기
-    receivedData += incomingByte; // 읽은 바이트를 누적
-
-    // 예를 들어, 특정 길이(예: 10바이트)가 되면 출력
-    if (receivedData.length() >= 10) {
-      Serial.print(receivedData); // 저장된 데이터 출력
-      receivedData = ""; // 데이터 초기화
+  while (Serial.available() > 0) 
+  {
+    char incomingByte = Serial.read();
+    if (incomingByte != -1) 
+    {
+      Serial.print(incomingByte);
     }
   }
-
-  // 추가적인 로직을 여기에 넣을 수 있습니다.
 }
--- src/rs485/rs485.py
+++ src/rs485/rs485.py
@@ -2,6 +2,7 @@ import time
 import struct
 import serial
 import serial.rs485
+from MD400 import MD400
 
 SERIAL_PORT = '/dev/ttyACM0'
 BAUDRATE = 19200
@@ -10,54 +11,35 @@ class RS485Communication:
     def __init__(self):
         self.ser = serial.rs485.RS485(SERIAL_PORT, baudrate=BAUDRATE, timeout=1)
         self.ser.rs485_mode = serial.rs485.RS485Settings()
-        self.RMID = 0xb7
-        self.TMID = 0xb8
-        self.ID = 0x01
-
-    def create_cmd_packet(self, RMID, TMID, ID, PARAMETER_ID, DATA_NUMBER, *DATA):
-        check_sum = RMID + TMID + ID + PARAMETER_ID + DATA_NUMBER + sum(DATA)
-        check_sum = (256 - (check_sum % 256)) % 256
-        fmt = 'B' * (len(DATA) + 6)
-        
-        packet_obj = struct.pack(fmt, 
-            RMID,
-            TMID,
-            ID,
-            PARAMETER_ID,
-            DATA_NUMBER,
-            *DATA,
-            check_sum)
-        return packet_obj
+        self.complete = False
 
     def read_data(self):
         if self.ser.readable():
             response = bytearray()
             response.extend(self.ser.read())
-            if(response and response[0] == self.TMID):
+            if(response and response[0] == md400.RMID):
                 response.extend(self.ser.read(4))
                 additional_read = response[-1] + 1
                 response.extend(self.ser.read(additional_read))
-                print("Received:    ", self.bytes_to_decimal(response))
+                print("Received:    ", *response)
+                self.complete = True
 
     def send_data(self, data):
         try:
             self.ser.write(data)
-            print("Sent         ", self.bytes_to_decimal(data))
+            print("Sent         ", *data)
         except Exception as e:
             print(f"Error sending data: {e}")
-
-
-    def bytes_to_decimal(self, byte_string):
-        return [byte for byte in byte_string]
 
     def run(self):
         while True:
             self.read_data()
-
-            packet = self.create_cmd_packet(self.RMID, self.TMID, self.ID, 10, 3, 8)
-            # packet = self.create_cmd_packet(183, 184, 1, 244, 4, 200, 0, 0, 0)
-            # packet = self.create_cmd_packet(1, 2, 3, 10, 4, 5, 6, 10, 0)
-            self.send_data(packet)
+            if(not self.complete):
+                packet = md400.set_alarm_reset()
+                packet = md400.set_pos(100)
+                # packet = self.create_cmd_packet(183, 184, 1, 244, 4, 200, 0, 0, 0)
+                # packet = self.create_cmd_packet(1, 2, 3, 10, 4, 5, 6, 10, 0)
+                self.send_data(packet)
 
     def close(self):
         self.ser.close()
@@ -65,6 +47,10 @@ class RS485Communication:
 
 if __name__ == '__main__':
     comm = RS485Communication()
+    md400 = MD400()
+    md400.RMID = 0xb7
+    md400.TMID = 0xb8
+    md400.ID = 0x01
     try:
         comm.run()
     finally:
