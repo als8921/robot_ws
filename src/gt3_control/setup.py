54d4d3ada26e3cebc3ba4d4b6aee521d3f3652bb
als8921 <als8921@naver.com>
2024-10-21 17:17:19 +0900

    chore : submodule 으로 모듈 받아오기

--- src/gt3_control/setup.py
+++ src/gt3_control/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'gt3_control'
+submodules = "gt3_control/submodules"
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, submodules],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -21,7 +22,9 @@ setup(
     entry_points={
         'console_scripts': [
             "server = gt3_control.server:main",
-            "client = gt3_control.client:main"
+            "client = gt3_control.client:main",
+            "control = gt3_control.lift_control:main",
+
         ],
     },
 )
