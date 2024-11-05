8a7bbf940e4331754f9e1b3c470ecec58cd2a416
als8921 <als8921@naver.com>
2024-11-05 15:37:24 +0900

    chore :목적지 위치 변경

--- src/actuator_control/actuator_control/MultiActuator.py
+++ src/actuator_control/actuator_control/MultiActuator.py
@@ -13,7 +13,7 @@ SERIAL_PORT = '/dev/ttyUSB0'    # 시리얼 포트
 BAUD_RATE = 57600               # 전송 속도
 ACTUATOR_IDS = [3, 0]           # 액츄에이터 ID 목록
 OPEN_POSITIONS = [3500, 0]   # 개방 위치
-CLOSED_POSITIONS = [0, 4250]   # 개방 위치
+CLOSED_POSITIONS = [0, 4100]   # 개방 위치
 ERROR_BOUNDARY = 10             # 위치 오차 허용 범위
 
 class CommandSubscriber(Node):
